26f61f232e1ea18935a06f4dbe307754b327616b
Kinan Mohrez <kinanmohrez2015@gmail.com>
2022-06-24 11:13:01 +0200

    use a prop for the header

--- src/List.jsx
+++ src/List.jsx
@@ -28,7 +28,7 @@ class List extends React.Component {
     return (
       <div className="col-6 mx-auto">
         {/*Replace the code below to call the title prop*/}
-        {this.props.title}
+        {this.props.title} 
         <form onSubmit={this.handleSubmit}>
           <label>
             <input
